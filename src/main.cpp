c327fe4a15bcad4c7c78eec0a1cf54918927ff1f
Evan Hodge <eahodge1@proton.me>
2026-04-01 19:44:43 -0700

    Hello Rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -106,19 +106,29 @@ int main()
     glDeleteShader(fragmentShader);
 
     // Storing vertex data on the graphics card
-    float verticies[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+    float vertices[] = {
+         0.5f, 0.5f, 0.0f,  // top right
+         0.5f,-0.5f, 0.0f,  // bottom right
+        -0.5f,-0.5f, 0.0f,  // bottom left
+        -0.5f,0.5f,0.0f   // top left
     };
 
-    unsigned int VBO, VAO;
+    unsigned int indicies[] = {
+        0, 1, 3,    // First triangle
+        1, 2, 3     // Second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -140,7 +150,8 @@ int main()
         // drawing
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // Events and buffer swapping
         glfwPollEvents();
@@ -148,6 +159,7 @@ int main()
     }
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     
     glfwTerminate();
